19014ef499dbed4d90cfa7b97fd5b8d393b3892c
root <piotr62@live.fr>
2022-06-03 02:08:38 -0700

    Task 8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints a single line using printf
- * Return: return 0
+ * Return: return 1
  */
 
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
