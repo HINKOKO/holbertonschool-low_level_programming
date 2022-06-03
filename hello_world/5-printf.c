cd6f71322a8c038d3c4ae111b31f11f59be8ee00
root <piotr62@live.fr>
2022-06-03 00:55:50 -0700

    Task 5 come on

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints a single line using puts
+ * Return: return 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
