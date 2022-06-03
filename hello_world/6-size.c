8d9b8db684dcba7026b1e3f0140cce81f6ca300f
root <piotr62@live.fr>
2022-06-03 01:31:07 -0700

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("Size of a char:%zu\n", sizeof(char));
-	printf("Size of an int:%zu\n", sizeof(int));
-	printf("Size of a long int:%zu\n", sizeof(long int));
-	printf("Size of a long long int:%zu\n", sizeof(long long int));
-	printf("Size of a float:%zu\n", sizeof(float));
+	printf("Size of a char:%d\n", sizeof(char));
+	printf("Size of an int:%d\n", sizeof(int));
+	printf("Size of a long int:%d\n", sizeof(long int));
+	printf("Size of a long long int:%d\n", sizeof(long long int));
+	printf("Size of a float:%d\n", sizeof(float));
 	return (0);
 }
 
