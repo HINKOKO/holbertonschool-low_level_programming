59d5987d846cb7942eab46c79ca753484953e10e
root <piotr62@live.fr>
2022-06-03 01:05:28 -0700

    fixing betty exigences

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints single line with puts
+ * Return: return value 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
