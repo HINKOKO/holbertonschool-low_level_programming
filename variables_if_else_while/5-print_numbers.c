b937cd7c79bbd5b1fbec0720e17700b1f750df58
root <piotr62@live.fr>
2022-06-06 04:45:48 -0700

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - displays all digit numbers starting from 0 to 10
+ * Return: always 0, success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 0)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
