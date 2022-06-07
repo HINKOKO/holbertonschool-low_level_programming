064b60090d7665ed01c7d89db7a163e08b6c7ce9
root <piotr62@live.fr>
2022-06-07 00:30:25 -0700

    test0-2

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[8] = "_putchar";
+	int i = 0;
+
+	while (text[i] != '\0')
+	{
+		_putchar(text[i++]);
+	}
+
 	_putchar('\n');
 	
 	return (0);
