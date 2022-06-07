24412269d822205378a7665395e964f80082e5b3
root <piotr62@live.fr>
2022-06-07 00:26:06 -0700

    test0-1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
- * main - print "_putchar" using function putchar, included in main.h
- * Return: 0 for success.
+ * main - Print "_putchar" using our function _putchar, included in main.h.
+ *
+ * Return: 0 (succes).
  */
 
 int main(void)
 {
 	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
+	
 	return (0);
 }
