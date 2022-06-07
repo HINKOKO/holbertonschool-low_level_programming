7a5463a022e9a1b69f88980d7905c20f75ea09ab
root <piotr62@live.fr>
2022-06-07 03:05:31 -0700

    Task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase
+ *Return: 0 for success
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
+
