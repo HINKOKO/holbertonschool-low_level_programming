9f86e9a1ebd494d8c3571b68b47af2227165a55a
root <piotr62@live.fr>
2022-06-08 01:04:20 -0700

    testing last advanced task

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - displays fibonacci suite numbers
+ * Return: exit success with 0
+ */
+
+int main(void)
+{
+	long int x = 1, y = 2, f;
+	int i = 0;
+
+	printf("%li, %li", x, y);
+	while (i < 96)
+	{
+		f = x + y;
+		printf(", %li", f);
+		x = y;
+		y = f;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
