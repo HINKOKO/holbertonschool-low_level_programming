eaa3e9fbb0ef1d6fce4dde245ed5b94b294fb0dc
root <piotr62@live.fr>
2022-06-09 00:06:33 -0700

    chinese numbers byebye

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9 with ascii code
+ * ch: character
+ *
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for(i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i!= '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
