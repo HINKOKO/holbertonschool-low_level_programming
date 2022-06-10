cfca0a9a86f0c7b00a3a5923885fe72dedc5ff76
root <piotr62@live.fr>
2022-06-10 08:09:09 -0700

    print number whatever

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -8,20 +8,14 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
+		n = -n;
 	}
-	else
+	if (n / 10)
 	{
-		n1 = n;
+		_putchar(n / 10);
 	}
-	if (n1 / 10)
-	{
-		_putchar(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	_putchar((n % 10) + '0');
 }
