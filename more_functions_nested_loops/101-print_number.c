51d468e1b0895b3899d746a3fa034b1d5bf9b3d8
root <piotr62@live.fr>
2022-06-10 08:23:33 -0700

    print number whatever

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
 /**
- * print_number - print an integer whatever n is
- * @n, @n1: integer procedure
+ * print_number - print an integer whatever is
+ * @n: integer switch
  *
  */
 
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
+		n1 = -n;
+	} else
 	{
-		_putchar(n / 10);
+		n1 = n;
 	}
-	_putchar((n % 10) + '0');
+
+	if (n1 / 10)
+	{
+		_putchar(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
