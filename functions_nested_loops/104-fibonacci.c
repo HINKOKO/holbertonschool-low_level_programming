1540510c9328455ef4c76997eb609d4a5082512c
root <piotr62@live.fr>
2022-06-10 07:52:07 -0700

    advanced 104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 
 	printf("%lu", a);
 
-	for (i = 1; i < 92; i++) /* unsigned long can't store more we have to plit */
+	for (i = 1; i < 91; i++) /* unsigned long can't store more we have to plit */
 	{
 		printf(", %lu", fib);
 		fib = fib + a;
@@ -26,7 +26,7 @@ int main(void)
 	fib_head = fib / 1000000000;
 	fib_tail = fib % 1000000000;
 
-	for (i = 92; i < 99; i++)
+	for (i = 91; i < 99; i++)
 	{
 		printf(", %lu", fib_head + (fib_tail / 1000000000));
 		printf("%lu", fib_tail % 1000000000);
