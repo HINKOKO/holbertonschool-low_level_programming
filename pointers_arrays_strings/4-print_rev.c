171d5a75e258cde8c73dc366d93b5de70986c7ab
root <piotr62@live.fr>
2022-06-13 00:14:33 -0700

    Isaac Asimov switched

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - takes a string and reverse it
+ * @s: the string to reverse
+ *
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+
