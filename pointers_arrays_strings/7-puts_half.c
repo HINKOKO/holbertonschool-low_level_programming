6234b47db6d434114812641b8a1a7d5770ea4369
root <piotr62@live.fr>
2022-06-13 03:43:43 -0700

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - determine lenght of a string
+ * len: the 'integer' for calculating string lenght
+ * Return: return the lenght
+ * @s: our string to calculate
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+  * puts_half - divide a string in half
+  * @str: the string to split
+  */
+
+void puts_half(char *str)
+{
+	int i, mid, leng;
+
+	leng = _strlen(str);
+	mid = (leng / 2);
+
+	for (i = mid; i < leng; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
