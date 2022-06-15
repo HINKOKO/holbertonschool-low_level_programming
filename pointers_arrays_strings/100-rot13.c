957d153609515c9fafca833ebcbccc10b1abe543
root <piotr62@live.fr>
2022-06-15 09:13:31 -0700

    rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - function to encode in rot13
+ * @c: the string/charac to convert
+ * Return: return the converted string
+ */
+
+char *rot13(char *c)
+{
+	char b[27] = "ABCDEFGHIJKLMabcdefghijklm";
+	char a[27] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+	int i, j;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; b[j] != '\0'; j++)
+		{
+			if (c[i] == b[j])
+				c[i] = a[j];
+		}
+	}
+	return (c);
+}
