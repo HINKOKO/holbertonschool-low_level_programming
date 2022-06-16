23c8a10bc39da0b61b0ad906d07abc4628a716d3
root <piotr62@live.fr>
2022-06-16 04:10:29 -0700

    task even more

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - function that copies tht value of b, into
+ * each of the first n character of the object pointed
+ * by si, perform copy operation byte by byte
+ * Return: return a pointer to the memory area s
+ * @n: the n bytes to fill with b
+ * @b: constant byte to be filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+	{
+		*s++ = b;
+	}
+	return (s);
+}
+
