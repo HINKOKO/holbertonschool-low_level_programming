443f8ad500cac3c0d3491caa2b7cdd1af3dbb772
root <piotr62@live.fr>
2022-06-21 02:57:30 -0700

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * Return: 0 successfull
+ * @argc: argument count
+ * @argv: one-dim array of string
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
