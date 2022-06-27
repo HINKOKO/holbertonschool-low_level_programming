05e1f01465079c95534d38a48832f6b6f22cdbf8
root <piotr62@live.fr>
2022-06-27 00:30:15 -0700

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < len2)
 		s3 = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		s3 = malloc(sizeof(char) * (len1 + len2 + n + 1));
+		s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
 		return (NULL);
 	while (i < len1)
