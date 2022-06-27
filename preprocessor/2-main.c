33021f4727602790ee1051461181d7c125aca6df
root <piotr62@live.fr>
2022-06-27 06:47:19 -0700

    Task 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * it was compiled from
+ * Return: 0 successfully successfull
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
