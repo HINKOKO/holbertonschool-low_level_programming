defef625aec86708877f425205bef3aef68749b0
root <piotr62@live.fr>
2022-06-26 23:26:45 -0700

    Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * Return: a pointer to the allocated memory
+ * @b: the size of memory to be allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,2 @@
+Folder purpose: how to use the exit function and what are the functions calloc and realloc and how to use them.
+
