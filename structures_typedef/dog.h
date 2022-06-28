bb98ce7fbd4a400c537d814e6c55faf7d6d8cb51
root <piotr62@live.fr>
2022-06-28 06:08:42 -0700

    tst syntax of typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: its age
  */
 
-typedef struct dog
+struct dog
 {
 	 char *name;
 	 char *owner;
 	 float age;
-}dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
