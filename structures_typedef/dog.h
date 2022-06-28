178a0e90310af5fc4c1ddec9d3db4021e3b52507
root <piotr62@live.fr>
2022-06-28 01:45:38 -0700

    Task 0 Woof,Woof git add dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
+#ifndef _DOG_
+#define _DOG_
+
+/**
+  * struct dog - defines a new structure
+  * for dog specificities woofi
+  * @name: name of poppy
+  * @owner: poppy master
+  * @age: poppy's age
+  */
+
+struct dog
+{
 	 char *name;
 	 char *owner;
 	 float age;
 };
+#endif
