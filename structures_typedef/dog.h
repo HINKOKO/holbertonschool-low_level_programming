91fe6929ac5302eabe076c26b1910775ef4e844e
root <piotr62@live.fr>
2022-06-28 23:51:34 -0700

    HArmonizing my dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: its age
  */
 
-struct dog
+typedef struct dog
 {
 	 char *name;
 	 char *owner;
 	 float age;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
