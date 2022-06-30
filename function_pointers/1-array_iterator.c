d61db7a36bb6f10ad4c65806bebab22e9b1029ea
root <piotr62@live.fr>
2022-06-29 23:32:38 -0700

    fixing case NULL

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,7 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	if (action != NULL || array != NULL)
+	if (action == NULL || array == NULL)
+		return;
+	else
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
