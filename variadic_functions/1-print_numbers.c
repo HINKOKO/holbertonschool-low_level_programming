794dad0704e9a71aaa3aafc9c541b71f3abbace1
root <piotr62@live.fr>
2022-07-03 23:16:37 -0700

    numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,16 +2,17 @@
 #include "variadic_functions.h"
 
 /**
- *
- *
- *
- *
+ * print_numbers - print numbers passed in
+ * paramaters, followed by new line
+ * @separator: string to be printed between numbers
+ * @n: number of parameters
  *
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+
 	va_list(num);
 
 	va_start(num, n);
@@ -19,6 +20,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
+			printf("%d", va_arg(num, int));
+		else if (i == 0)
 			printf("%d", va_arg(num, int));
 		else
 			printf("%s%d", separator, va_arg(num, int));
