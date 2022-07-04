63db963e76361c5508b27394f8cb41a4d486f8aa
root <piotr62@live.fr>
2022-07-04 06:36:39 -0700

    Coolest advanced !

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,16 @@
+section .data
+	text db "Hello, World", 10
+
+section .text
+	global main
+
+main:
+mov rax, 1
+mov rdi, 1
+mov rsi, text
+mov rdx, 12 + 1
+syscall
+
+mov rax, 60
+mov rdi, 0
+syscall
