f82c7763fc80bc83e6af304bdbf3713332a79a87
root <piotr62@live.fr>
2022-07-05 05:36:05 -0700

    Task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,45 +3,78 @@
 #include "variadic_functions.h"
 
 /**
- *print_all - function that print anything
- *@format: list of types of arguments passed to function
- *
- *
+ *print_c - print a char
+ *@c: the character to print
+ */
+
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+/**
+ *print_i - print an integer
+ *@i: integer to print
+ */
+void print_i(va_list i)
+{
+	printf("%i", va_arg(i, int));
+}
+/**
+ *print_f - print a float num
+ *@f: the float to print
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+/**
+ *print_s - print a string
+ *@s: string to print
+ */
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char*);
+
+	if (str == NULL)
+		printf("(nil)");
+	printf("%s", str);
+}
+/**
+ *print_all - prints anything whatever cifs
+ *@format: the format to check and print
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str;
-
+	unsigned int i, j;
 	va_list list;
+
+	prt pick[] = {
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
+		{0, NULL},
+	};
 
 	va_start(list, format);
 
-	while (format[i])
+
+	i = 0;
+	while (format && format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (pick[j].c != 0)
 		{
-			case 'c':
-				printf("%c", va_arg(list, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(list, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(list, double));
-				break;
-			case 's':
-				str = va_arg(list, char*);
-				if (!str)
-					str = "(nil)";
-				printf("%s", str);
-				break;
-			default:
-				i++;
-				continue;
+			if (pick[j].c == format[i])
+			{
+				pick[j].f(list);
+				if (format[i + 1])
+				{
+					printf(", ");
+				}
+				j++;
+			}
 		}
 		i++;
 	}
-	printf("\n");
-	va_end(list);
 }
