60c03722f2f9de4d5360e443e214ec6e5382619f
root <piotr62@live.fr>
2022-07-13 04:57:15 -0700

    Task 7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - find the nth node o fthe list
+ *@index: index of the node to be find
+ *@head: pointer to starting point of list
+ *return: the nth node of the list
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ride_the_list;
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	ride_the_list = head;
+
+	for (i = 0; i < index; i++)
+	{
+		if (ride_the_list->next == NULL)
+			return (NULL);
+		ride_the_list = ride_the_list->next;
+	}
+	return (ride_the_list);
+}
+
