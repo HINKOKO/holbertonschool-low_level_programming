584cae4ab6b284f8c26baf29cc8b9fac9f0afc77
root <piotr62@live.fr>
2022-07-14 05:35:18 -0700

    Task Nine bro'

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp = *head;
 
 	new = malloc(sizeof(listint_t));
+
+	if (!new || !head)
+		return (NULL);
+
 	new->n = n;
 	new->next = NULL;
 
