91c52d8ccb96c2324974f8b9560758ab8328871b
root <piotr62@live.fr>
2022-07-21 07:34:27 -0700

    Task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -31,7 +31,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		current = current->next;
 	}
 	current->prev->next = current->next;
-	current->next->prev = current->prev;
+	if (current->next)
+		current->next->prev = current->prev;
 	free(current);
 	return (1);
 }
