63c458f746d1ae82f6562f3fc18f3144be98febd
root <piotr62@live.fr>
2022-08-19 04:12:05 -0700

    checkin'

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,9 +15,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_table_t *));
+	table->array = malloc(size * sizeof(hash_node_t *));
 	if (!table->array)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
