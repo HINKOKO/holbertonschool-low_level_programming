d814c59a60df0d5283598cc7d340cedd0fdb35c0
root <piotr62@live.fr>
2022-08-19 09:48:47 -0700

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,12 +5,12 @@
  *
  */
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int hash_table_set_koko(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index = 0;
 	hash_node_t *tmp, *new;
 
-	if (!key || strlen(key) == 0)
+	if (!key || strlen(key) == 0 || !ht || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
