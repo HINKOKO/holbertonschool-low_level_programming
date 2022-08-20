26cc6a0030342553c60fb302d95ebb4f3a2e69ac
root <piotr62@live.fr>
2022-08-20 05:48:05 -0700

    Task 5 cooler than 3

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to be printed
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *printer;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		printer = ht->array[i];
+		while (printer != NULL)
+		{
+			printf("'%s': '%s', ", printer->key, printer->value);
+			printer = printer->next;
+		}
+	}
+	printf("}\n");
+}
