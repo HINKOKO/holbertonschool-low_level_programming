e42331ffbe0203df8ddc270379e19c33a4825693
root <piotr62@live.fr>
2022-08-21 01:55:58 -0700

    giving a test for advanced stuff

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -3,6 +3,7 @@
 /**
  * shah_table_create - create a sorted hash table PHP elephant you my friend
  * @size: size of sorted hash table
+ * Return: the sorted hash table we create
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
@@ -27,8 +28,10 @@ shash_table_t *shash_table_create(unsigned long int size)
 }
 
 /**
- * @brief
- *
+ * shash_node_t - give birth to a new node for our sorted hash table
+ * @key: key for value
+ * @value: to be stored
+ * Return: NULL for failure or the new node if successfull
  */
 shash_node_t *make_new_snode(const char *key, const char *value)
 {
@@ -53,10 +56,39 @@ shash_node_t *make_new_snode(const char *key, const char *value)
 	node->next = node->snext = node->sprev = NULL;
 	return (node);
 }
+
 /**
- * @brief
- *
+ * sorted_insert - add a newnode in a sorted hash table,
+ * in a sorted way to the linked list (sorted by ASCII's values)
+ * @ht: the sorted hash table
+ * @node: the node to add
  */
+
+void sorted_insert(shash_table_t *ht, shash_node_t *node)
+{
+	shash_node_t *browser;
+
+	if (ht->shead == NULL && ht->stail == NULL)
+	{
+		ht->shead = ht->stail = node;
+		return;
+	}
+	browser = ht->shead;
+	while (browser->snext != NULL && browser->snext->value < node->value)
+	{
+		browser = browser->snext;
+	}
+	node->snext = browser->snext;
+	browser->snext = node;
+}
+/**
+ * shash_table_set - sets key to value in a sorted hash table
+ * @ht: the hash table we modify
+ * @key: is the key of data
+ * @value: data to push
+ * Return: 1 for success, 0 otherwise
+ */
+
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
@@ -65,4 +97,133 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || strlen(key) == 0 || !value)
 		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	mover = ht->array[index];
+	while (mover)
+	{
+		if (strcmp(mover->key, key) == 0)
+		{
+			new = strdup(value);
+			if (!new)
+				return (0);
+			free(mover->value);
+			mover->value = new;
+			return (1);
+		}
+		mover = mover->snext;
+	}
+	newnode = make_new_snode(key, value);
+	if (!newnode)
+		return (0);
+	newnode->snext = ht->array[index];
+	ht->array[index] = newnode;
+	sorted_insert(ht, newnode);
+	return (1);
+}
+
+/**
+ * shash_table_get - gets a value associated with a key
+ * @ht: sorted hash table we look into
+ * @key: the key we are lookin' to
+ * Return: value associated with key or NULL if we fail to find
+ */
+
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	shash_node_t *temp;
+
+	if (!ht || !key || strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->snext;
+	}
+	return (NULL);
+}
+
+/**
+ * shash_table_print - prints a sorted hash table
+ * @ht: sorted hash table
+ */
+
+void shash_table_print(const shash_table_t *ht)
+{
+	unsigned int i;
+	shash_node_t *sprinter;
+	char status = 0; /* trick to skip last comma */
+
+	if (!ht || !ht->array)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		sprinter = ht->shead;
+		while (sprinter != NULL)
+		{
+			if (status == 1)
+				printf(", ");
+			printf("'%s': '%s'", sprinter->key, sprinter->value);
+			status = 1;
+			sprinter = sprinter->snext;
+		}
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_print_rev - print sorted hash table in reverse
+ * @ht: sorted hash table to be printed
+ */
+
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	unsigned int i;
+	shash_node_t *sprinter;
+	char status = 0; /* trick to skip last comma */
+
+	if (!ht || !ht->array)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		sprinter = ht->stail;
+		while (sprinter != NULL)
+		{
+			if (status == 1)
+				printf(", ");
+			printf("'%s': '%s'", sprinter->key, sprinter->value);
+			status = 1;
+			sprinter = sprinter->sprev;
+		}
+	}
+	printf("}\n");
+}
+
+void shash_table_delete(shash_table_t *ht)
+{
+	unsigned int i;
+	shash_node_t *messiah;
+
+	if (!ht || !ht->array || !ht->size)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			messiah = ht->array[i]->next;
+			ht->array[i] = messiah;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
