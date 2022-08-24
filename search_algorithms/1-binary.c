49a3378ff9a3adcc036bc0d346f40f6121ef6629
root <piotr62@live.fr>
2022-08-24 02:28:46 -0700

    was is that?

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -39,13 +39,13 @@ int binary_search(int *array, size_t size, int value)
 		left = 0;
 		right = size - 1;
 		print_array(array + left, right + 1 - left);
-		while (left < right)
+		while (left <= right)
 		{
 			mid = (left + right) / 2;
 			if (array[mid] < value)
 				left = mid + 1;
 			else if (array[mid] > value)
-				right = mid;
+				right = mid - 1;
 			else
 				return (mid);
 			print_array(array + left, right + 1 - left);
